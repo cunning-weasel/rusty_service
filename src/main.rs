22dfc58236dfa2bf12ca5fb66f9eca65a803404d
paul <paulbongers22@gmail.com>
2022-07-11 21:57:50 +0200

    added basics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+// bringing in filesystem into scope
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -13,10 +15,37 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    // hand-coding HTTP request and res
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
+    // hard-code data for / req to get var,
+    // transfor get into btye string - then check
+    // if buffer starts with bytes in get
+    let get = b"GET / HTTP/1.1\r\n";
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("hello.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        // hard-coded 404 req
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!(
+            "{}\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len(),
+            contents
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
-
